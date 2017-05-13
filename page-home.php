f6ae75481badb426a2c6b4079c6391210d3dcb2a
St John Karp <?@stjo.hn>
2017-05-13 06:59:39 -0700

    Correct spelling and lightly copy edit the home page
    I've fixed a bunch of spelling issues and done a light copy edit
    of the home page:
    - Standardized the spelling. There was a mix of US and British
      English. Most of the site seems to be US, so I used that as
      the standard.
    - Standardized the names of products and services with reference
      to their own websites (StatusNet, diaspora*, etc.).
    - Did a light copy edit for readability and style.
    - Tweaked punctuation so lists end with full stops and we get some
      proper em-dashes in there.
    - Fixed misc. typos and spelling errors.

--- page-home.php
+++ page-home.php
@@ -6,19 +6,19 @@
                     <div class="flex flex-3">
                         <article>
                             <header>
-                                <h3>Decentralisation</h3>
+                                <h3>Decentralization</h3>
                             </header>
-                            <p>Decentralised architecture with no central authority or ownership</p>
-                            <p>Relationships can be made across any compatible system, creating a network of internet scale made up of smaller sites</p>
-                            <p>Seamless wall-to-wall posts and remote comments, even across different network nodes</p>
+                            <p>Decentralized architecture with no central authority or ownership.</p>
+                            <p>Relationships can be made across any compatible system, creating a network of Internet scale made up of smaller sites.</p>
+                            <p>Seamless wall-to-wall posts and remote comments, even across different network nodes.</p>
                         </article>
                         <article>
                             <header>
                                 <h3>Privacy</h3>
                             </header>
-                            <p>Access list on every items</p>
-                            <p>Private conversation groups - on these pages all communications are restricted to group members</p>
-                            <p>One-to-one private messaging on supported protocols</p>
+                            <p>Access lists for every item.</p>
+                            <p>Private conversation groups &mdash; on these pages all communications are restricted to group members.</p>
+                            <p>One-to-one private messaging on supported protocols.</p>
                             <p>Optionally "expire" old content after a certain period of time.</p>
                             <p>Download your personal data. It all belongs to you.</p>
                         </article>
@@ -26,10 +26,10 @@
                             <header>
                                 <h3>Interoperability</h3>
                             </header>
-                            <p>Built-in support for status.net, GNU-social, quitter, Diaspora</p>
-                            <p>Email contacts and communications supported (two-way) via IMAP4rev1/ESMTP</p>
-                            <p>Arbitrary websites and blogs may be imported into your social stream via RSS/Atom feeds</p>
-                            <p>Plugin support to other services</p>
+                            <p>Built-in support for StatusNet, GNU social, Quitter, and diaspora*.</p>
+                            <p>Support for email contacts and communications (two-way) via IMAP4rev1/ESMTP.</p>
+                            <p>Import arbitrary websites and blogs into your social stream via RSS/Atom feeds.</p>
+                            <p>Support for other services via plugins.</p>
                         </article>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
                 <div class="inner">
                     <header>
                         <h2>A personal social network</h2>
-                        <p>Keep in contact only with people you care</p>
+                        <p>Keep in contact with people you care about.</p>
                     </header>
                     <div class="flex flex-4">
                         <div class="box person">
@@ -62,14 +62,14 @@
                 <i class="fa fa-calendar" aria-hidden="true"></i>
                             </div>
                             <h3>Events</h3>
-                            <p>Organize next meeting of your reading group, or let everybody know about your cool party!</p>
+                            <p>Organize the next meeting of your reading group, or let everybody know about your cool party!</p>
                         </div>
                         <div class="box person">
                             <div class="image round">
                 <i class="fa fa-users" aria-hidden="true"></i>
                             </div>
                             <h3>Profiles</h3>
-              <p>Like in real life, have a public profile and private aspects only for your closest friends. Or keep everything private!</p>
+                            <p>Like in real life, have a public profile and private aspects only for your closest friends. Or keep everything private!</p>
                         </div>
                     </div>
                     <footer>
@@ -82,7 +82,7 @@
             <section id="three" class="wrapper special">
                 <div class="inner">
                     <header class="align-center">
-                        <h2>Free software - Free protocols</h2>
+                        <h2>Free software &mdash; Free protocols</h2>
                         <p>Friendica is free software and use free protocols.</p>
                     </header>
                     <div class="flex flex-2">
@@ -93,8 +93,8 @@
                             <header>
                                 <h3>PHP &amp; MySQL</h3>
                             </header>
-                            <p>Run your personal Friendica node on shared hosts</p>
-                            <p>Friendica runs on PHP with MySQL as database. If you can run Wordpress, you can run Friendica.</p>
+                            <p>Run your personal Friendica node on shared hosts.</p>
+                            <p>Friendica runs on PHP with MySQL as a database. If you can run WordPress, you can run Friendica.</p>
                             <footer>
                                 <a href="/requirements/" class="button special">More</a>
                             </footer>
@@ -106,8 +106,8 @@
                             <header>
                                 <h3>Extensible</h3>
                             </header>
-                            <p>Extensible via third-party plugin modules and themes.</p>
-                            <p>Add functionalities, personalize look and feel, connect to thirty-part serivces.</p>
+                            <p>Extensible via third-party plugins and themes.</p>
+                            <p>Add functionality, personalize the look and feel, connect to third-party services.</p>
                             <footer>
                                 <a href="https://github.com/friendica/friendica-addons" class="button special">More</a>
                             </footer>
